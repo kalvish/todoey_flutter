19059f9be8a60aef3607426127df12ec3bbe29d4
Kalan Nawarathne <kalanvishwanath@gmail.com>
2019-09-29 18:17:40 +0530

    list view builder added

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,48 +1,27 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkBoxCallback;
 
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
-
-  void checkBoxStateFunc(bool checkBoxState) {
-    setState(() {
-      isChecked = checkBoxState;
-    });
-  }
+  TaskTile({this.isChecked, this.taskTitle, this.checkBoxCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(
-        'This is a task',
-        style: TextStyle(
-          decoration: isChecked ? TextDecoration.lineThrough : null,
+        title: Text(
+          taskTitle,
+          style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null,
+          ),
         ),
-      ),
-      trailing: new TaskCheckBox(
-          checkBoxState: isChecked, checkBoxStateFunc: checkBoxStateFunc),
-    );
-  }
-}
-
-class TaskCheckBox extends StatelessWidget {
-  final bool checkBoxState;
-  final Function checkBoxStateFunc;
-
-  TaskCheckBox({this.checkBoxState, this.checkBoxStateFunc});
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      activeColor: Colors.lightBlueAccent,
-      value: checkBoxState,
-      onChanged: (newValue) {
-        checkBoxStateFunc(newValue);
-      },
-    );
+        trailing: Checkbox(
+          activeColor: Colors.lightBlueAccent,
+          value: isChecked,
+          onChanged: (newValue) {
+            checkBoxCallback(newValue);
+          },
+        ));
   }
 }
